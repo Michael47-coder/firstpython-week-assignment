ea1417097473fcae9a9be6b30d57937a7555e7e5
Michael47-coder <michaeldmathew47@gmail.com>
2025-03-14 04:51:08 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,23 @@
+#Get user input
+num1=float(input("Enter the first number"))
+num2=float(input("Enter the second number"))
+operation=input("Enter an operator (+,-,*/)")
+        
+        #Perform the operation
+        if operation=="+":
+        result=num1+num2
+        print(f"{num1}+{num2}={result}")
+
+        elif operation=="-";
+        result=num1-num2
+        print(f"{num1}-{num2}={result}")
+
+        elif operation=="*";
+        result=num1*num2
+        print(f"{num1}*{num2}={result}")
+ 
+        elif operation=="/";
+        result=num1/num2
+        print(f"{num1}/{num2}={result}")
+elif num2 !=0;
+ result=num1/num2
